d59b8a9cd266413f0b141a85dcddc2b5911b2a91
MatthewRChambers <matthewchambersuk@gmail.com>
2025-01-31 15:40:33 +0000

    Added Roxygen documentation to stage_assigning function

--- R/stage_assigning.R
+++ R/stage_assigning.R
@@ -1,16 +1,23 @@
-example_stages = c("Stage1", "Stage2", "Stage3", "Stage4", "Stage5")
-
-example_data = dplyr::tribble(
-  ~ID, ~ Stage1, ~Stage2, ~Stage3, ~Stage4, ~Stage5,
-  1, "01/01/2000", NA, NA, NA, NA,
-  2, "01/01/2000", "02/01/2000", NA, NA, NA,
-  3, "01/01/2000", "02/01/2000", "03/01/2000", NA, NA,
-  4, "01/01/2000", "02/01/2000", "03/01/2000", "04/01/2000", NA,
-  5, "01/01/2000", "02/01/2000", "03/01/2000", "04/01/2000", "05/01/2000",
-  6, NA, NA, NA, NA, NA,
-)
-
-
+#' Finds the latest recorded developmental stage
+#'
+#' @param columns A character vector specifying the column names of each stage in order.
+#' @param data The data frame containing information of date an individual reached a specific developmental stage with one row for each individual.
+#'
+#' @returns A new column "last_observed_stage" which contains the last stage found with a date in it.
+#' @export
+#'
+#' @examples
+#' example_stages = c("Stage1", "Stage2", "Stage3", "Stage4", "Stage5")
+#' example_data = dplyr::tribble(
+#' ~ID, ~ Stage1, ~Stage2, ~Stage3, ~Stage4, ~Stage5,
+#'   1, "01/01/2000", NA, NA, NA, NA,
+#'   2, "01/01/2000", "02/01/2000", NA, NA, NA,
+#'   3, "01/01/2000", "02/01/2000", "03/01/2000", NA, NA,
+#'   4, "01/01/2000", "02/01/2000", "03/01/2000", "04/01/2000", NA,
+#'   5, "01/01/2000", "02/01/2000", "03/01/2000", "04/01/2000", "05/01/2000",
+#'   6, NA, NA, NA, NA, NA,
+#' )
+#' stage_assigning(columns = example_stages, data = example_data)
 stage_assigning <- function(columns, data) {
   data |>
     dplyr::mutate(
